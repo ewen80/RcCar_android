e31bad8224c9fbcdccbd373efcc09c97483fb0f1
wenliang <ewen80@gmail.com>
2018-09-20 16:38:57 +0800

    1

--- app/src/main/java/pw/ewen/mycar/video/rtp/RtpSingleNalPacket.java
+++ app/src/main/java/pw/ewen/mycar/video/rtp/RtpSingleNalPacket.java
@@ -12,6 +12,7 @@ public class RtpSingleNalPacket extends RtpPacket {
         super(rtpBytes);
     }
 
+    //返回除了rtp头以外的所有内容,包含nal头
     @Override
     public byte[] getNal() {
         return this.rtpPayload;
